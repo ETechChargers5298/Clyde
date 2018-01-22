49ac140d8a0bf0728fbe7425c08005b081c0748d
admin <admin@10.217.224.56>
2018-01-22 09:48:17 -0500

    Mr. Morris' helping code

--- Clyde/src/org/usfirst/frc/team5298/robot/autonomous/StartLeft.java
+++ Clyde/src/org/usfirst/frc/team5298/robot/autonomous/StartLeft.java
@@ -9,13 +9,19 @@ public class StartLeft extends CommandGroup {
 	
 	public StartLeft(char scaleSide) {
 		if(scaleSide == 'L') {
-			addSequential(new MoveLinear(5.0, 0.8)); 
+			addSequential(new MoveLinear(7.0, 0.8)); 
 			addSequential(new MoveRotate(90, 0.5));
+			addSequential(new MoveLinear(2.0, 0.8));
 		}
 		
 		else if(scaleSide == 'R') {
 			addSequential(new MoveLinear(5.0, 0.8));
 			addSequential(new MoveRotate(90, 0.5));
+			addSequential(new MoveLinear(5.0, 0.8));
+			addSequential(new MoveRotate(0, 0.5));
+			addSequential(new MoveLinear(2.0, 0.8));
+			addSequential(new MoveRotate(-90, 0.5));
+			addSequential(new MoveLinear(2.0, 0.8));
 		}
 	}
 }
--- Clyde/src/org/usfirst/frc/team5298/robot/autonomous/StartRight.java
+++ Clyde/src/org/usfirst/frc/team5298/robot/autonomous/StartRight.java
@@ -1,18 +1,27 @@
 package org.usfirst.frc.team5298.robot.autonomous;
 
 import org.usfirst.frc.team5298.robot.commands.MoveLinear;
+import org.usfirst.frc.team5298.robot.commands.MoveRotate;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class StartRight extends CommandGroup {
 	
 	public StartRight(char scaleSide) {
-		if(scaleSide == 'L') {
-			addSequential(new MoveLinear(5.0, 0.8)); 
+		if(scaleSide == 'R') {
+			addSequential(new MoveLinear(7.0, 0.8)); 
+			addSequential(new MoveRotate(-90, 0.5));
+			addSequential(new MoveLinear(2.0, 0.8));
 		}
 		
-		else if(scaleSide == 'R') {
-			addSequential(new MoveLinear(5.0, 0.8)); 
+		else if(scaleSide == 'L') {
+			addSequential(new MoveLinear(5.0, 0.8));
+			addSequential(new MoveRotate(-90, 0.5));
+			addSequential(new MoveLinear(5.0, 0.8));
+			addSequential(new MoveRotate(0, 0.5));
+			addSequential(new MoveLinear(2.0, 0.8));
+			addSequential(new MoveRotate(90, 0.5));
+			addSequential(new MoveLinear(2.0, 0.8));
 		}
 	}
 }
