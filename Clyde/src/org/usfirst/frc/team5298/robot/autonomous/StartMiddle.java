668ea9a041f1913afc26815264a0b3ebac4b0bbf
admin <admin@10.217.224.56>
2018-01-22 09:50:13 -0500

    Mr. Morris' helping code

--- Clyde/src/org/usfirst/frc/team5298/robot/autonomous/StartMiddle.java
+++ Clyde/src/org/usfirst/frc/team5298/robot/autonomous/StartMiddle.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team5298.robot.autonomous;
 
 import org.usfirst.frc.team5298.robot.commands.MoveLinear;
+import org.usfirst.frc.team5298.robot.commands.MoveRotate;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -8,11 +9,23 @@ public class StartMiddle extends CommandGroup {
 	
 	public StartMiddle(char scaleSide) {
 		if(scaleSide == 'L') {
-			addSequential(new MoveLinear(5.0, 0.8)); 
+			addSequential(new MoveLinear(3.0, 0.8));
+			addSequential(new MoveRotate(-90, 0.5));
+			addSequential(new MoveLinear(3.0, 0.8));
+			addSequential(new MoveRotate(0, 0.5));
+			addSequential(new MoveLinear(3.0, 0.8));
+			addSequential(new MoveRotate(90, 0.5));
+			addSequential(new MoveLinear(2.0, 0.8));
 		}
 		
 		else if(scaleSide == 'R') {
-			addSequential(new MoveLinear(5.0, 0.8)); 
+			addSequential(new MoveLinear(3.0, 0.8)); 
+			addSequential(new MoveRotate(90, 0.5));
+			addSequential(new MoveLinear(3.0, 0.8));
+			addSequential(new MoveRotate(0, 0.5));
+			addSequential(new MoveLinear(3.0, 0.8));
+			addSequential(new MoveRotate(-90, 0.5));
+			addSequential(new MoveLinear(2.0, 0.8));
 		}
 	}
 }
